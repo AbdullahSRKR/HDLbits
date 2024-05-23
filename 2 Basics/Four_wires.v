41e2a0e3c4c8ba2c539045042173b30a02de091e
AbdullahSRKR <168886300+AbdullahSRKR@users.noreply.github.com>
2024-05-23 16:35:16 +0530

    Add files via upload

---
+++ 2 Basics/Four_wires.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.05.2024 14:37:21
+// Design Name: 
+// Module Name: Four_wires
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Four_wires(input a,b,c, output w,x,y,z);
+assign w=a;
+assign x=b;
+assign y=b;
+assign z=c;
+
+endmodule
